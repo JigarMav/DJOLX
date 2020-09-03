258e6a16d020b8cec5ffbf4cc5f7abececb21f5f
Jigar Mav <jbhanushali237@gmail.com>
2020-09-03 10:01:24 +0530

    Added the basic node server structure code

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log("in the middleware ");
+  next();
+});
 
 const server = http.createServer(app);
 server.listen(3000);
